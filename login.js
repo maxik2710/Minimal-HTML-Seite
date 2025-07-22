1e9fadf13519a06974a6ee15fff20081ef14e01b
maxik2710 <vertex.adpetitor@gmail.com>
2025-07-22 21:19:57 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,21 @@
-// Get Elements
+// Elemente holen
 const loginBtn = document.getElementById('loginBtn');
 const modal = document.getElementById('loginModal');
 const closeModal = document.getElementById('closeModal');
 
-// Open Modal on Button Click
+// Login-Modal öffnen
 loginBtn.onclick = function() {
   modal.style.display = "block";
 }
 
-// Close Modal on Close Button Click
+// Login-Modal schließen (über ×)
 closeModal.onclick = function() {
   modal.style.display = "none";
 }
 
-// Close Modal on Click Outside Modal
+// Login-Modal schließen (Klick außerhalb)
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = "none";
   }
 }
